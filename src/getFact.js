aa908c12fe2df98f1aa5c08191d01fa7e06736cd
Matthew Ferry <matthewferry@github.com>
2020-08-14 02:37:41 -0600

    Adds fallback when param missing

--- src/getFact.js
+++ src/getFact.js
@@ -1,5 +1,5 @@
 const queryString = window.location.search;
 const urlParams = new URLSearchParams(queryString);
-const fact = urlParams.get('fact')
+const fact = urlParams.has('fact') ? urlParams.get('fact') : 'it’s just a string';
 
 export default fact;
